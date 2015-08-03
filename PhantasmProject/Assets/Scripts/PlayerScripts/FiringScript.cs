8c1f1df63dda3b493f46da036fba7975c8a1160e
ckinne1771 <ckinney1771@gmail.com>
2015-08-03 16:56:07 +0100

    Added Bullet Firing
    TODO (for v0.01 alpha): Fix bug where multiple bullets fire at the same
    time (add timer most likely to fix), Add player boundaries, add enemies.

---
+++ PhantasmProject/Assets/Scripts/PlayerScripts/FiringScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FiringScript : MonoBehaviour {
+	public GameObject bulletPrefab;
+	GameObject Clone;
+	 
+
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKey (KeyCode.Mouse0)) {
+			Clone = (Instantiate (bulletPrefab, transform.position, transform.rotation)) as GameObject;
+		}
+	}
+}
