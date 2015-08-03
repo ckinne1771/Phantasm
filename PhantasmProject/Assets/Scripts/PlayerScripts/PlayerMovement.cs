9c4cb8c3ee8eac059958add7856a41a3469d7132
ckinne1771 <ckinney1771@gmail.com>
2015-08-03 14:49:40 +0100

    Added Movement and player rotation
    TODO: Firing Bullets, player boundaries and enemies.

---
+++ PhantasmProject/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public float speed = 10.0f;
+
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKey (KeyCode.A)) {
+			transform.position = new Vector3((transform.position.x - 1.0f * speed *Time.deltaTime), transform.position.y, transform.position.z);
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			transform.position = new Vector3((transform.position.x + 1.0f * speed *Time.deltaTime), transform.position.y, transform.position.z);
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+			transform.position = new Vector3(transform.position.x , (transform.position.y - 1.0f * speed *Time.deltaTime), transform.position.z);
+		}
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position = new Vector3(transform.position.x , (transform.position.y + 1.0f * speed *Time.deltaTime), transform.position.z);
+		}
+
+	}
+}
