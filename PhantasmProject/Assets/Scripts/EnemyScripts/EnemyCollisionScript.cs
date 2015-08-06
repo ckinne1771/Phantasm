10f3304f3d5c8a735eac0df9b5e0d5f32813b9a7
ckinne1771 <ckinney1771@gmail.com>
2015-08-06 15:12:15 +0100

    Added more spawn points, bullet destroys enemies
    TODO: Add scoring, refine spawning, refine boundaries, add scenes

---
+++ PhantasmProject/Assets/Scripts/EnemyScripts/EnemyCollisionScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyCollisionScript : MonoBehaviour {
+
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D (Collision2D other)
+	{
+		Debug.Log ("collision");
+		if (other.gameObject.tag == "bullet") {
+			Destroy (gameObject);
+		}
+	}
+}
