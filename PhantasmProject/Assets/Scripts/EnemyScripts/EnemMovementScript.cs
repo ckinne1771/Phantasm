09b30fbdb4ca6aadae410df2e7698b0bbd894fdf
ckinne1771 <ckinney1771@gmail.com>
2015-09-07 15:57:05 +0100

    Movement Fixed, Art added
    TODO: Add sound, background art, explosion animation

--- PhantasmProject/Assets/Scripts/EnemyScripts/EnemMovementScript.cs
+++ PhantasmProject/Assets/Scripts/EnemyScripts/EnemMovementScript.cs
@@ -5,7 +5,14 @@ public class EnemMovementScript : MonoBehaviour {
 	public float speed = 5.0f;
 	// Update is called once per frame
 	void Update () {
-		transform.LookAt (GameObject.Find ("PlayerBody").transform.position);
-		transform.Translate (Vector3.forward*speed*Time.deltaTime);
+		Vector3 playerPos = GameObject.Find ("Player").transform.position;
+
+		Quaternion rotation = Quaternion.LookRotation (playerPos - transform.position, transform.TransformDirection (Vector3.up));
+		transform.rotation = Quaternion.Slerp (transform.rotation, new Quaternion(0, 0, rotation.z, rotation.w), speed*Time.deltaTime);
+		//Debug.Log (transform.rotation.ToString ());
+
+		transform.position = Vector3.MoveTowards (transform.position, playerPos, speed * Time.deltaTime);
+	
+
 	}
 }
